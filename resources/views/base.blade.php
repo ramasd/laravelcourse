f92ce13a1c973e633536cd8fe399585cc60117d9
ramasd <ramunas.danupas@gmail.com>
2019-05-18 18:40:50 +0300

    First commit. Paruoštas pagrindinis views šablonas base.blade.php, pagal kurį sukurti home, search, about ir contact šablonai ir sukurti routai jiems bei sukurtas style.css failas

---
+++ resources/views/base.blade.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+  <title>{{ $meta_title ?? 'Krepšinio aikštelės' }}</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  </head>
+  <body>
+    <div id="wrapper">
+      <div id="header">
+        <a id="logo" href="{{ url('/') }}">KREPŠINIO AIKŠTELĖS</a>
+      </div>
+      <div id="main_menu">
+        <ul>
+          <li><a href="{{ url('/') }}">Pagrindinis</a></li>
+          <li><a href="{{ url('aiksteles') }}">Aikštelių paieška</a></li>
+          <li><a href="{{ url('apie') }}">Apie projektą</a></li>
+          <li><a href="{{ url('kontaktai') }}">Kontaktai</a></li>
+        </ul>
+        <div class="clear"></div>
+      </div>
+      <div id="content">
+        @yield('content')
+      </div>
+      <div id="footer">
+        © {{ date('Y') }}
+      </div>
+    </div>
+  </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function() {
+    return view('home');
+});
+Route::get('aiksteles', function() {
+    return view('search');
+});
+Route::get('apie', function() {
+    return view('about');
+});
+Route::get('kontaktai', function() {
+    return view('contact');
+});
+
