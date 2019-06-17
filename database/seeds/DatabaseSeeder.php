bee147668d46a0f3862982bfe871f6de13c4714c
ramasd <ramunas.danupas@gmail.com>
2019-06-17 11:34:33 +0300

    7. Duomenų bazės migracijos. Created cities, courts and types migrations files and modified DatabaseSeeder.php file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Type;
+use App\City;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Eloquent::unguard();
+
+        Type::create(
+            array(
+                'title' => 'lauko aikštelė',
+            )
+        );
+        Type::create(
+            array(
+                'title' => 'sporto salė',
+            )
+        );
+        Type::create(
+            array(
+                'title' => 'privati aikštelė',
+            )   
+        );
+
+        City::create(
+            array(
+                'title' => 'Vilnius',
+            )
+        );
+        City::create(
+            array(
+                'title' => 'Kaunas',
+            )
+        );
+        City::create(
+            array(
+                'title' => 'Klaipėda',
+            )
+        );
+        City::create(
+            array(
+                'title' => 'Zarasai',
+            )
+        );
+
     }
 }
